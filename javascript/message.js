25cfff753a51d3f6a125444de8fff6b569f6b51c
rajalakshmi <poboxrajalakshmi@gmail.com>
2024-10-03 13:40:54 +0530

    ok

---
+++ javascript/message.js
@@ -0,0 +1,10 @@
+function msg1(){
+    alert("Rabiah Mateen Sayed");
+    }
+   function msg2(){
+        alert("SICSR");
+        }
+       function msg3(){
+            alert("BCA");
+            }
+    // document.write("Hello JavaScript by JavaScript");
